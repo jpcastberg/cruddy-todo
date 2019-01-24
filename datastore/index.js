8d54c53e8219c57f4fb460e406c3154916cec3ac
jpcastberg <jpcastberg@gmail.com>
2019-01-23 20:00:31 -0800

    Complete all index.js functions up to refactoring 'readAll' to expect correct todo text

--- datastore/index.js
+++ datastore/index.js
@@ -27,40 +27,59 @@ exports.create = (text, callback) => {
 };
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  fs.readdir(exports.dataDir, (err, arr) => {
+    if (err) {
+      console.log('error reading all');
+    } else {
+      var arr2 = [];
+      for (var i = 0; i < arr.length; i++) {
+        var id = arr[i].slice(0, -4);
+        var text = arr[i].slice(0, -4);
+        var todo = { id, text };
+        arr2.push(todo);
+      }
+      callback(null, arr2);
+    }
   });
-  callback(null, data);
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  fs.readFile(exports.dataDir + '/' + id + '.txt', (err, text) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      text = text.toString('utf8');
+      var todo = { id, text };
+      callback(null, todo);
+    }
+  });
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  exports.readOne(id, function(err, todo) {
+    if (err) {
+      callback(err);
+    } else {
+      fs.writeFile(exports.dataDir + '/' + id + '.txt', text, (err) => {
+        if (err) {
+          throw ('error writing counter');
+        } else {
+          callback(null, text);
+        }
+      });  
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(exports.dataDir + '/' + id + '.txt', (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback(null, id);
+      console.log(exports.dataDir + '/' + id + '.txt was deleted');
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
