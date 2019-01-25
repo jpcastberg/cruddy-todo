0be1a2be865497686866d2af63a111debe36a427
jpcastberg <jpcastberg@gmail.com>
2019-01-24 16:22:43 -0800

    Complete refactor of 'readAll' to use Promise.all

--- datastore/index.js
+++ datastore/index.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+const Promise = require('bluebird');
 
 var items = {};
+
+Promise.promisifyAll(fs);
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
@@ -26,23 +29,6 @@ exports.create = (text, callback) => {
   });   
 };
 
-exports.readAll = (callback) => {
-  fs.readdir(exports.dataDir, (err, arr) => {
-    if (err) {
-      console.log('error reading all');
-    } else {
-      var arr2 = [];
-      for (var i = 0; i < arr.length; i++) {
-        var id = arr[i].slice(0, -4);
-        var text = arr[i].slice(0, -4);
-        var todo = { id, text };
-        arr2.push(todo);
-      }
-      callback(null, arr2);
-    }
-  });
-};
-
 exports.readOne = (id, callback) => {
   fs.readFile(exports.dataDir + '/' + id + '.txt', (err, text) => {
     if (err) {
@@ -54,6 +40,43 @@ exports.readOne = (id, callback) => {
     }
   });
 };
+
+var readOneAsync = Promise.promisify(exports.readOne);
+
+exports.readAll = (callback) => {
+  fs.readdirAsync(exports.dataDir)
+    .then(function(filenames) {
+      var proms = _.map(filenames, function(filename) {
+        var id = filename.slice(0, -4);
+        return fs.readFileAsync(exports.dataDir + '/' + filename)
+          .then(function(fileContents) {
+            var text = fileContents.toString('utf8');
+            return {id, text};
+          });
+      });
+      Promise.all(proms)
+        .then((values) => {
+          console.log('LOOK', values);
+          callback(null, values);
+        });
+    });
+  // fs.readdir(exports.dataDir, (err, arr) => {
+  //   if (err) {
+  //     console.log('error reading all');
+  //   } else {
+  //     var arr2 = [];
+  //     for (var i = 0; i < arr.length; i++) {
+  //       var id = arr[i].slice(0, -4);
+  //       var text = arr[i].slice(0, -4);
+  //       var todo = { id, text };
+  //       arr2.push(todo);
+  //     }
+  //     callback(null, arr2);
+  //   }
+  // });
+};
+
+
 
 exports.update = (id, text, callback) => {
   exports.readOne(id, function(err, todo) {
